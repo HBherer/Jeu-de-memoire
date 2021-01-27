e0f5b7d90dc18fc79dd1e5b47ca6d5c57f1a677e
HansBherer <77289920+HansBherer@users.noreply.github.com>
2021-01-27 15:10:05 -0500

    Début prog script validation
    J'ai commancé à faire le script de la validation de la page de configuration

---
+++ script.js
@@ -0,0 +1,14 @@
+const form = document.getElementById("formConfig")
+form.addEventListener("submit, valider")
+
+function valider(e){
+    const messages = []
+    let nbCarte = document.inscription.nbCarte.value
+    nbCarte = // reste à le mettre en nombre paire tout le temps
+}
+
+
+if (Message.length > 0) {
+    e.preventDefault()
+    const div = document.getElementById("message")
+}
