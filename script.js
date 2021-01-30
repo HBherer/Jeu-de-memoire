0748a6ae63f98220cb80f166837e33c9b0143596
H.Bherer[Laptop] <77289920+HansBherer@users.noreply.github.com>
2021-01-30 12:21:22 -0500

    Ajouter nb pair de carte

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
+const nombrePaires = 10
+const nbTours = nombrePaires // J'avais fait une division par deux ici
+const tableauCartes = []
+for (let i = 0; i < nbTours; i++) {
+  tableauCartes.push(i)
+  tableauCartes.push(i)
+}
+
+const tableauCartesMelangees = []
+while(tableauCartes.length > 0) {
+  const index = Math.floor(Math.random() * tableauCartes.length)
+  tableauCartesMelangees.push(tableauCartes[index])
+  tableauCartes.splice(index, 1)
+  console.log(tableauCartes)
+  console.log(tableauCartesMelangees)
+
+}
+
+
+
 const form = document.getElementById("formConfig")
 form.addEventListener("submit, valider")
 
